5033547d00ccd4bb9260422130a41bf1d9b3d3df
Guilherme Medeiros <guimargoni@gmail.com>
2024-10-10 22:33:19 -0300

    10_10

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,86 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace MarvelYouTubeTracker
 {
-    static void Main(string[] args)
+    public class Comment
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        public string CommenterName { get; private set; }
+        public string Text { get; private set; }
+
+        public Comment(string commenterName, string text)
+        {
+            CommenterName = commenterName;
+            Text = text;
+        }
+    }
+
+    public class Video
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public int LengthInSeconds { get; private set; }
+        private List<Comment> _comments;
+
+        public Video(string title, string author, int lengthInSeconds)
+        {
+            Title = title;
+            Author = author;
+            LengthInSeconds = lengthInSeconds;
+            _comments = new List<Comment>();
+        }
+
+        public void AddComment(Comment comment)
+        {
+            _comments.Add(comment);
+        }
+
+        public int GetNumberOfComments()
+        {
+            return _comments.Count;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Length: {LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+            foreach (var comment in _comments)
+            {
+                Console.WriteLine($"- {comment.CommenterName}: {comment.Text}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video1 = new Video("How to Use Mjolnir", "Thor Odinson", 600);
+            var video2 = new Video("Tech Innovations at Stark Industries", "Tony Stark", 900);
+            var video3 = new Video("The Wakanda Experience", "T'Challa", 1200);
+
+            video1.AddComment(new Comment("Loki", "I could wield Mjolnir if I wanted to."));
+            video1.AddComment(new Comment("Hulk", "Mjolnir too small for Hulk!"));
+            video1.AddComment(new Comment("Jane Foster", "Great explanation, Thor!"));
+
+            video2.AddComment(new Comment("Pepper Potts", "Proud of you, Tony."));
+            video2.AddComment(new Comment("Rhodey", "Can't wait to try the new suit."));
+            video2.AddComment(new Comment("Happy Hogan", "When can I get my upgrade?"));
+
+            video3.AddComment(new Comment("Shuri", "You forgot to mention my inventions!"));
+            video3.AddComment(new Comment("Okoye", "Wakanda Forever!"));
+            video3.AddComment(new Comment("Nakia", "Great video, T'Challa."));
+
+            List<Video> videos = new List<Video> { video1, video2, video3 };
+
+            foreach (var video in videos)
+            {
+                video.DisplayInfo();
+            }
+        }
     }
 }
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,9 +1,160 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace MarvelOrderSystem
 {
-    static void Main(string[] args)
+    public class Product
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        private string _Name;
+        private string _ProductID;
+        private double _Price;
+        private int _Quantity;
+        private double _Discount;
+
+        public Product(string name, string productID, double price, int quantity, double discount = 0)
+        {
+            _Name = name;
+            _ProductID = productID;
+            _Price = price;
+            _Quantity = quantity;
+            _Discount = discount;
+        }
+
+        public double GetTotalCost()
+        {
+            return (_Price * _Quantity) * (1 - (_Discount / 100));
+        }
+
+        public string GetPackingLabel()
+        {
+            return $"{_Name} (ID: {_ProductID})";
+        }
+
+        public double GetDiscount()
+        {
+            return _Discount;
+        }
+    }
+
+    public class Address
+    {
+        private string _Street;
+        private string _City;
+        private string _State;
+        private string _Country;
+
+        public Address(string street, string city, string state, string country)
+        {
+            _Street = street;
+            _City = city;
+            _State = state;
+            _Country = country;
+        }
+
+        public bool IsInUSA()
+        {
+            return _Country == "USA";
+        }
+
+        public string GetFullAddress()
+        {
+            return $"{_Street}\n{_City}, {_State}\n{_Country}";
+        }
+    }
+
+    public class Customer
+    {
+        private string _Name;
+        private Address _Address;
+
+        public Customer(string name, Address address)
+        {
+            _Name = name;
+            _Address = address;
+        }
+
+        public bool IsInUSA()
+        {
+            return _Address.IsInUSA();
+        }
+
+        public string GetShippingLabel()
+        {
+            return $"{_Name}\n{_Address.GetFullAddress()}";
+        }
+    }
+
+    public class Order
+    {
+        private List<Product> _Products = new List<Product>();
+        private Customer _Customer;
+
+        public Order(Customer customer)
+        {
+            _Customer = customer;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _Products.Add(product);
+        }
+
+        public double GetTotalPrice()
+        {
+            double total = 0;
+            foreach (var product in _Products)
+            {
+                total += product.GetTotalCost();
+            }
+
+            double shippingCost = _Customer.IsInUSA() ? 5.0 : 35.0;
+            return total + shippingCost;
+        }
+
+        public string GetPackingLabel()
+        {
+            string label = "Packing Label:\n";
+            foreach (var product in _Products)
+            {
+                label += $"{product.GetPackingLabel()}\n";
+            }
+            return label;
+        }
+
+        public string GetShippingLabel()
+        {
+            return $"Shipping Label:\n{_Customer.GetShippingLabel()}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address starkAddress = new Address("10880 Malibu Point", "Malibu", "CA", "USA");
+            Customer tonyStark = new Customer("Tony Stark", starkAddress);
+
+            Address wakandaAddress = new Address("Golden City", "Birnin Zana", "Wakanda", "Wakanda");
+            Customer tChalla = new Customer("T'Challa", wakandaAddress);
+
+            Product ironManSuit = new Product("Iron Man Suit", "MK-50", 3500000.99, 1);
+            Product vibraniumShield = new Product("Vibranium Shield", "SHIELD-001", 1500000.75, 1);
+            Product arcReactor = new Product("Arc Reactor", "ARC-002", 2500000.50, 1);
+
+            Order order1 = new Order(tonyStark);
+            order1.AddProduct(ironManSuit);
+            order1.AddProduct(arcReactor);
+
+            Order order2 = new Order(tChalla);
+            order2.AddProduct(vibraniumShield);
+
+            Console.WriteLine(order1.GetPackingLabel());
+            Console.WriteLine(order1.GetShippingLabel());
+            Console.WriteLine($"Total Price: {order1.GetTotalPrice():C}");
+
+            Console.WriteLine(order2.GetPackingLabel());
+            Console.WriteLine(order2.GetShippingLabel());
+            Console.WriteLine($"Total Price: {order2.GetTotalPrice():C}");
+        }
     }
 }
