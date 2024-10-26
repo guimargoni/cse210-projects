9e0d82f9443c76e9162c08609745f2dfecf85338
Guilherme Medeiros <guimargoni@gmail.com>
2024-10-26 11:28:39 -0300

    Learning 06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.Color}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
